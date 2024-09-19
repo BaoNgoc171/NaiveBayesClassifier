ef9b36e900f2b3a9274822a18582120de75ee558
BaoNgoc171 <baongocecopark@gmail.com>
2024-09-20 00:12:33 +0200

    cứu

--- src/NaiveBayes.java
+++ src/NaiveBayes.java
@@ -1,19 +1,54 @@
 import java.io.File;
 import java.io.IOException;
 import java.net.PortUnreachableException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class NaiveBayes {
+    private List<WordCounter> wordCounters;
+    private int totalNumberSpam;
+    private int totalNumberNonSpam;
+    private int totalDocument;
+    private double spamScore;
+    private double nonSpamScore;
+
     /*
     String [] is an array of focusWords, ghi rõ số lượng private WordCounter objects
     => each WordCounter respond 1 String from focusWords
      */
-    public NaiveBayes(String[] focusWords) {}
+    public NaiveBayes(String[] focusWords) {
+        this.totalNumberSpam = 0;
+        this.totalNumberNonSpam = 0;
+        this.totalDocument = 0;
+
+        this.wordCounters = new ArrayList<>();
+        for (String focusWord : focusWords) {
+            wordCounters.add(new WordCounter(focusWord));
+        }
+    }
 
     /* document be processed by all WordCounter object manage by NaiveBayes object
     NaiveBayes object should have: total number of spam / non spam documents has seen
     Calculate spam or no spam score P spam (non-spam) = # spam(non-spam) document seen / # total documents seen
      */
-    public void addSample(String document) {}
+    public void addSample(String document) {
+        try (Scanner scanner = new Scanner(Paths.get(document))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                this.totalDocument ++;
+                if (line.contains("1")) {
+                    this.totalNumberSpam ++;
+                } else {
+                    this.totalNumberNonSpam ++;
+                }
+            }
+        }
+        this.spamScore = (double)this.totalNumberSpam / this.totalDocument;
+        this.nonSpamScore = (double)this.totalNumberNonSpam / this.totalDocument;
+
+    }
 
     /*
     Take over document without classification and determine probability between the document
@@ -25,6 +60,8 @@ public class NaiveBayes {
     -> Compare whether no spam score < spam score
      */
     public boolean classify(String unclassifiedDocument) {
+        this.spamScore = this.spamScore.add
+        this.nonSpamScore = (double)this.totalNumberNonSpam / this.totalDocument;
         return true;
     }
 
