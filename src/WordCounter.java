cafede3c49f51c5c9ce1dbbf7d6dd08e32ccf448
Anh Tu Duong <anhtu.duong@studenti.unitn.it>
2024-09-20 12:31:12 +0200

    fix and clear class WordCounter

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -2,54 +2,87 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.nio.file.Paths;
 public class WordCounter {
-    private String focus;
-    private int wordNoSpam;
-    private int wordSpam;
-    private int timesFocusSpam;
-    private int timesFocusNoSpam;
+    private String focusWord;
+    private int totalWordsInNoSpam;
+    private int totalWordInSpam;
+    private int totalFocusWordsInNoSpam;
+    private int totalFocusWordsInSpam;
     private double probabilityNoSpam;
     private double probabilitySpam;
-    //xác định focusWord, ko return j cả
+
+    /* Constructor, initialize the variables
+    * @param String focusWord
+    */
     public WordCounter(String focusWord) {
-        this.focus = focusWord;
-
+        this.focusWord = focusWord;
+        totalWordsInNoSpam = 0;
+        totalWordInSpam = 0;
+        totalFocusWordsInNoSpam = 0;
+        totalFocusWordsInSpam = 0;
     }
 
-    //trả lại focusWord mỗi khi gọi
+    /* Get the current focus word
+    * @return focusWord
+    */
     public String getFocusWord() {
-        return this.focus;
+        return this.focusWord;
     }
 
-    /* xác định spam (1 hay 0); số lần focusWord xuất hiện ở spam and non spam
-     số lần focusWord xuất hiện
-     lọc spam hay non spam, xác định bộ đếm int,đọc từng line và mỗi line lọc từng từ
-     và ghi vào bộ đếm
-
-     */
+    /* When adding document sample, we need to update
+    * the counting total number of words and the focus words
+    * respecting spam or no spam of the document sample.
+    * @param String document sample
+    * */
     public void addSample(String document) {
-        this.wordSpam = 0;
-        this.wordNoSpam = 0;
-        this.timesFocusNoSpam = 0;
-        this.timesFocusSpam = 0;
-        String[] words = document.split(" ");
-        if (document.startsWith("1")) {
-            for (int i = 1; i < words.length; i++) {
-                wordSpam ++;
-                if (words[i].equals(this.focus)) {
-                    timesFocusSpam++;
-                }
-            }
+        String documentContent = getDocumentContent(document);
+        if (isSpam(document)) {
+            this.totalWordInSpam += getTotalWordCount(documentContent);
+            this.totalFocusWordsInSpam += getTotalFocusWordCount(documentContent);
         } else {
-            for (int i = 1; i < words.length; i++) {
-                wordNoSpam ++;
-                if (words[i].equals(this.focus)) {
-                        timesFocusNoSpam++;
-                    }
-                }
-            }
-
+            this.totalWordsInNoSpam += getTotalWordCount(documentContent);
+            this.totalFocusWordsInNoSpam += getTotalFocusWordCount(documentContent);
+        }
     }
 
+    /* Get the document actual word content,
+    * filter out the identifier 0/1 at the
+    * beginning of the document.
+    *
+    * @param String document
+    * @return String the actual word content
+    * */
+    private String getDocumentContent(String document) {
+        return document.substring(2);
+    }
+
+    /* Identify if the document is spam or no spam
+    * @param String document
+    * @return boolean spam or no spam
+    */
+    private boolean isSpam(String document) {
+        return document.startsWith("1");
+    }
+
+    /* Get the total number of words in the document content
+    * @param String document content
+    * @return int total number of words
+    */
+    private int getTotalWordCount(String documentContent) {
+        return documentContent.split("\\s").length;
+    }
+
+    /* Get the frequency of the focus word
+    * appears in the document content.
+    *
+    * @param String document content
+    * @return int the frequency number
+    * */
+    private int getTotalFocusWordCount(String documentContent) {
+        // Split the string by the focus word and
+        // count the number of parts that were splited
+        String[] parts = documentContent.split(this.focusWord, -1);
+        return parts.length - 1;
+    }
 
     /*xác định wordCounter train bằng cách:
     xuất hiện ít nhất 1 lần => timesFocusSpam + timesFocusNoSpam >=1
@@ -58,34 +91,34 @@ public class WordCounter {
     => 2 and 3 holds=> 1 holds => lấy 2 và 3
 
      */
-    public boolean isCounterTrained() {
-        if (this.timesFocusSpam >= 1 && this.timesFocusNoSpam >= 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    /*
-    giả về giá trị probabilities bằng cách lấy
-    timeFocusNoSpam / wordNoSpam => no spam
-    timeFocusSpam / wordSpam => spam
-    nếu 2 method conditional được gọi trước isCounterTrained thì throw IllegalStateException
-
-     */
-    public double getConditionalNoSpam() {
-        if (isCounterTrained()) {
-            throw new IllegalStateException();
-        }
-        this.probabilityNoSpam = (double)timesFocusNoSpam / wordNoSpam;
-        return probabilityNoSpam ;
-    }
-    public double getConditionalSpam() {
-        if (isCounterTrained()) {
-            throw new IllegalStateException();
-        }
-        this.probabilitySpam = (double)timesFocusSpam / wordSpam;
-        return this.probabilitySpam;
-    }
+//    public boolean isCounterTrained() {
+//        if (this.timesFocusSpam >= 1 && this.timesFocusNoSpam >= 1) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//    /*
+//    giả về giá trị probabilities bằng cách lấy
+//    timeFocusNoSpam / wordNoSpam => no spam
+//    timeFocusSpam / wordSpam => spam
+//    nếu 2 method conditional được gọi trước isCounterTrained thì throw IllegalStateException
+//
+//     */
+//    public double getConditionalNoSpam() {
+//        if (isCounterTrained()) {
+//            throw new IllegalStateException();
+//        }
+//        this.probabilityNoSpam = (double)timesFocusNoSpam / wordNoSpam;
+//        return probabilityNoSpam ;
+//    }
+//    public double getConditionalSpam() {
+//        if (isCounterTrained()) {
+//            throw new IllegalStateException();
+//        }
+//        this.probabilitySpam = (double)timesFocusSpam / wordSpam;
+//        return this.probabilitySpam;
+//    }
 
     public static void main(String[] args) {
         WordCounter wc = new WordCounter("good");
